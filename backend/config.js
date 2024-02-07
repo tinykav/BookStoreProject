880a1254c16a679379aead63e98e5e8c54bf2d00
Tiny Handalage <kaveeshatiny@gmail.com>
2024-02-07 17:54:24 +0530

    Book Store Project using MERN stack initialized

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+   'mongodb+srv://root:root@books-store-mern.6upqdrz.mongodb.net/books-collection?retryWrites=true&w=majority';
+// 'mongodb+srv://root:root@books-store-mern.6upqdrz.mongodb.net/books-collection?retryWrites=true&w=majority';
---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { PORT,mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import{Book}from './models/bookModels.js';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app=express();
+
+//middleware for parsing request body
+app.use(express.json());
+
+//middleware for handling CORS POLICY
+//Option 1:Allow all orgins with default of cors(*)
+app.use(cors());
+//Option 2:Allow custom orgins
+// app.use(
+//   cors({
+//     orgin:'http://localhost:3000',
+//     method:['GET','POST','PUT','DELETE'],
+//     allowedHeaders:['Content-Type'],
+//   })
+// );
+
+
+app.get('/',(request,response)=>{
+    console.log(request)
+    return response.status(234).send('Welcome to MERN stack');
+});
+
+app.use('/books',booksRoute);
+
+
+
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log('App not connected to database');
+  });
